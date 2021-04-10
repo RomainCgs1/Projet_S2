5c65dbcd8aff23a996ae76f348b360b5ddbc732b
Romain <romain@cognasse.com>
2021-04-10 12:28:41 +0200

    On test javafx

--- src/main/java/fr/insa/empire/MainGraphApp.java
+++ src/main/java/fr/insa/empire/MainGraphApp.java
@@ -5,11 +5,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainGraphApp extends Application {
+
     @Override
     public void start(Stage stage) throws Exception
     {
         stage.setTitle("TestGraph");
-
         Scene scene = new Scene(new MainGraphique(), 600, 500);
         stage.setScene(scene);
         stage.centerOnScreen();
