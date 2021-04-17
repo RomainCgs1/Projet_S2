e782f7f1b0ad6ae27df661a98f35e27f1e82846f
Lumarochoco <romain.degrace@gmail.com>
2021-04-17 16:55:03 +0200

    Classe Identificateur EN COURS Méthodes save dans les différentes classes EN COURS

--- src/main/java/fr/insa/empire/treillis/Barre.java
+++ src/main/java/fr/insa/empire/treillis/Barre.java
@@ -1,5 +1,7 @@
 package fr.insa.empire.treillis;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.Set;
 
 public class Barre {
@@ -9,9 +11,8 @@ public class Barre {
     private Noeuds noeudDebut;
     private Noeuds noeudFin;
     private Type_de_barre type;
-    
-    
-    
+    // private Set<Noeuds> extrémités;
+
     //Constructeur
     public Barre(int id, Noeuds noeudDebut, Noeuds noeudFin, Type_de_barre type) {
         this.identificateur = id;
@@ -19,47 +20,58 @@ public class Barre {
         this.noeudFin = noeudFin;
         this.type = type;
     }
-    
+
     //Encapsulation
-    public Noeuds getNoeudDebut (){
+    public Noeuds getNoeudDebut() {
         return this.noeudDebut;
     }
-    
-    public Noeuds getNoeudFin (){
+
+    public Noeuds getNoeudFin() {
         return this.noeudFin;
     }
-    
-    public Type_de_barre getType (){
+
+    public Type_de_barre getType() {
         return this.type;
     }
-    
-    public void setNoeudDebut (Noeuds noeudDebut){
+
+    public void setNoeudDebut(Noeuds noeudDebut) {
         this.noeudDebut = noeudDebut;
     }
-    
-    public void setNoeudFin (Noeuds noeudFin){
+
+    public void setNoeudFin(Noeuds noeudFin) {
         this.noeudFin = noeudFin;
     }
-    
-    public void setType (Type_de_barre type){
+
+    public void setType(Type_de_barre type) {
         this.type = type;
     }
-    
+
     //to String 
-    public String toString (){
-        String s="";
-        s= s+"Barre selectionnee : "+this.identificateur +"\n";
-        s= s+"Type de la barre : "+ this.type+"\n";
-        s= s+"Noeud du debut "+this.noeudDebut+"\n";
-        s= s+"Noeud du fin "+this.noeudFin+"\n";
-        
+    public String toString() {
+        String s = "";
+        s = s + "Barre selectionnee : " + this.identificateur + "\n";
+        s = s + "Type de la barre : " + this.type + "\n";
+        s = s + "Noeud du debut " + this.noeudDebut + "\n";
+        s = s + "Noeud du fin " + this.noeudFin + "\n";
+
         return s;
     }
-  
-    public Set<Noeuds> extrémités;
 
-
-
-
+    public void save(BufferedWriter bW, Identificateur idNum) throws IOException {
+        //Format : BARRE/id/type/idpdébut/idpfin
+        if (!idNum.objetPresent(this.noeudDebut)) {
+            this.noeudDebut.save(bW, idNum);
+        }
+        if (!idNum.objetPresent(this.noeudFin)) {
+            this.noeudFin.save(bW, idNum);
+        }
+        bW.append("Barre/");
+        int id = idNum.getOrSetKey(this);
+        bW.append(id + "/");
+        Type_de_barre type = this.getType();
+        bW.append(type+"/");
+        bW.append(this.noeudDebut + "/");
+        bW.append(this.noeudFin + "/");
+    }
 
 }
