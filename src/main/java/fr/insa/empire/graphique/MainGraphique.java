ca40aed735328961b1b74ea8886469526ed087b6
Romain <romain@cognasse.com>
2021-04-17 16:40:21 +0200

    autre version de l'identificateur + code plus joli + menu étendu.

--- src/main/java/fr/insa/empire/graphique/MainGraphique.java
+++ src/main/java/fr/insa/empire/graphique/MainGraphique.java
@@ -110,6 +110,15 @@ public class MainGraphique extends BorderPane {
         );
 
 
+        //actione de barre
+        this.mtbBarre.setOnAction(
+                Action -> {
+                    etatNoeud = 0;
+                    tbNoeud.setText("Noeud");
+                }
+        );
+
+
         //Set up des actions du splitMenuButton
         choix1.setOnAction(new EventHandler<ActionEvent>() {
             @Override
