651d1260fee6cec94ab197c72e7d22f8d2e4b0a5
Lumarochoco <romain.degrace@gmail.com>
2021-04-18 17:23:05 +0200

    Save générale EN COURS

--- src/main/java/fr/insa/empire/treillis/Treillis.java
+++ src/main/java/fr/insa/empire/treillis/Treillis.java
@@ -3,7 +3,6 @@ package fr.insa.empire.treillis;
 import fr.insa.empire.utils.Identificateur;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -19,25 +18,37 @@ public class Treillis {
         //Format : TREILLIS
         //<objets contenus dans le treillis, 1 barre par ligne>
         //Fin Treillis
+        
         bW.append("Treillis \n");
         Map<Integer, Object> map = idNum.getKetToObject();
         Iterator it = map.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry<Integer, Integer> entry = (Map.Entry) it.next();
-
+            boolean bns = entry.getClass().isInstance(Noeud_simple.class);
+            boolean bna = entry.getClass().isInstance(Noeud_appui.class);
+            boolean bbarre = entry.getClass().isInstance(Barre.class);
+            boolean bSegT = entry.getClass().isInstance(Segment_terrain.class);
+            boolean bTriangleT = entry.getClass().isInstance(Triangle_terrain.class);
+            
+            if (bns==true){
+              //TODO  
+            }
+            if (bna==true){
+                //TODO  
+            }
+            if (bbarre==true){
+                //TODO  
+            }
+            if (bSegT==true){
+                //TODO  
+            }
+            if (bTriangleT==true){
+                //TODO  
+            }
         }
 
     }
 
-   /* public void saveCase(String nomClass, Identificateur idNum, Object entry) {
-
-        switch (nomClass) {
-            case (nomClass.contains("Noeud_simple"):
-                
-                if (!idNum.objetPresent(entry)) {
-            .save(bW, idNum);
-        }
-            
-        }*/
+   
     
 }
