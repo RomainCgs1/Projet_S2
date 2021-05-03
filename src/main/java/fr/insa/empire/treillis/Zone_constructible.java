7e4fa864916f40867c6589aa4d0ec47d6de9aced
achil <achil@DESKTOP-DTF09S2>
2021-05-03 11:20:08 +0200

    Zone_constructible, la véritable

--- src/main/java/fr/insa/empire/treillis/Zone_constructible.java
+++ src/main/java/fr/insa/empire/treillis/Zone_constructible.java
@@ -7,5 +7,24 @@ public class Zone_constructible {
     private double Ymin;
     private double Ymax;
     
-           
+     public Zone_constructible(double Xmin,double Xmax, double Ymin, double Ymax){
+         this.Xmin = Xmin;
+         this.Xmax = Xmax;
+         this.Ymin = Ymin;
+         this.Ymax = Ymax;
+     }      
+     
+     public double getXmin(){
+         return this.Xmin;
+     }
+     public double getXmax(){
+         return this.Xmax;
+     }
+     public double getYmin(){
+         return this.Ymin;
+     }
+     public double getYmax(){
+         return this.Ymax;
+     }
+     
 }
